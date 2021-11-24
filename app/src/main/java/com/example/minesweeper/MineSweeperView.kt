fcbb81cb28d5fb2e4cb9fb66f2e02e1871c2e7f0
Artis Brizs <artis.brizs@student.griffith.ie>
2021-11-24 08:44:34 +0000

    Milestone 1

---
+++ app/src/main/java/com/example/minesweeper/MineSweeperView.kt
@@ -0,0 +1,9 @@
+package com.example.minesweeper
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class MineSweeperView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
+
+}
